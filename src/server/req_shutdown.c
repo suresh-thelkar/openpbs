visheshh/pbspro

POC to make scheduler work with multiple server/s (#43)

src/server/req_shutdown.c
@@ -266,7 +266,7 @@ req_shutdown(struct batch_request *preq)
 		(void)failover_send_shutdown(FAILOVER_SecdShutdown);
 
 	if (shutdown_who & SHUT_WHO_SCHED)
-		(void)contact_sched(SCH_QUIT, NULL, pbs_scheduler_addr, pbs_scheduler_port);	/* tell scheduler to quit */
+		(void)contact_sched(SCH_QUIT, NULL,  dflt_scheduler, PRIMARY);	/* tell scheduler to quit */
 
 	if (shutdown_who & SHUT_WHO_SECDONLY) {
 		reply_ack(preq);

src/server/sched_func.c
@@ -281,7 +281,7 @@ action_sched_priv(attribute *pattr, void *pobj, int actmode)
 			}
 			psched = (pbs_sched*) GET_NEXT(psched->sc_link);
 		}
-		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched->pbs_scheduler_addr, psched->pbs_scheduler_port);
+		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched, PRIMARY);
 	}
 
 	return PBSE_NONE;
@@ -322,7 +322,7 @@ action_sched_log(attribute *pattr, void *pobj, int actmode)
 			}
 			psched = (pbs_sched*) GET_NEXT(psched->sc_link);
 		}
-		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched->pbs_scheduler_addr, psched->pbs_scheduler_port);
+		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched, PRIMARY);
 
 	}
 
@@ -406,6 +406,7 @@ poke_scheduler(attribute *pattr, void *pobj, int actmode)
 			if (pattr->at_val.at_long)
 				set_scheduler_flag(SCH_SCHEDULE_CMD, dflt_scheduler);
 		}
+		server.sv_attr[(int)SRV_ATR_State].at_flags |= ATR_VFLAG_MODCACHE;
 	} else {
 		if (actmode == ATR_ACTION_ALTER) {
 			if (pattr->at_val.at_long)
@@ -521,7 +522,7 @@ action_sched_partition(attribute *pattr, void *pobj, int actmode)
 				}
 			}
 		}
-		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, pin_sched->pbs_scheduler_addr, pin_sched->pbs_scheduler_port);
+		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, pin_sched, PRIMARY);
 	}
 
 	return PBSE_NONE;

suresh-thelkar/openpbs

src/server/req_shutdown.c
@@ -266,7 +266,7 @@ req_shutdown(struct batch_request *preq)
 		(void)failover_send_shutdown(FAILOVER_SecdShutdown);
 
 	if (shutdown_who & SHUT_WHO_SCHED)
-		(void)contact_sched(SCH_QUIT, NULL, pbs_scheduler_addr, pbs_scheduler_port);	/* tell scheduler to quit */
+		(void)contact_sched(SCH_QUIT, NULL,  dflt_scheduler, SECONDARY);	/* tell scheduler to quit */
 
 	if (shutdown_who & SHUT_WHO_SECDONLY) {
 		reply_ack(preq);

src/server/sched_func.c
@@ -281,7 +281,7 @@ action_sched_priv(attribute *pattr, void *pobj, int actmode)
 			}
 			psched = (pbs_sched*) GET_NEXT(psched->sc_link);
 		}
-		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched->pbs_scheduler_addr, psched->pbs_scheduler_port);
+		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched, SECONDARY);
 	}
 
 	return PBSE_NONE;
@@ -322,7 +322,7 @@ action_sched_log(attribute *pattr, void *pobj, int actmode)
 			}
 			psched = (pbs_sched*) GET_NEXT(psched->sc_link);
 		}
-		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched->pbs_scheduler_addr, psched->pbs_scheduler_port);
+		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, psched, SECONDARY);
 
 	}
 
@@ -406,6 +406,7 @@ poke_scheduler(attribute *pattr, void *pobj, int actmode)
 			if (pattr->at_val.at_long)
 				set_scheduler_flag(SCH_SCHEDULE_CMD, dflt_scheduler);
 		}
+		server.sv_attr[(int)SRV_ATR_State].at_flags |= ATR_VFLAG_MODCACHE;
 	} else {
 		if (actmode == ATR_ACTION_ALTER) {
 			if (pattr->at_val.at_long)
@@ -521,7 +522,7 @@ action_sched_partition(attribute *pattr, void *pobj, int actmode)
 				}
 			}
 		}
-		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, pin_sched->pbs_scheduler_addr, pin_sched->pbs_scheduler_port);
+		(void)contact_sched(SCH_ATTRS_CONFIGURE, NULL, pin_sched, SECONDARY);
 	}
 
 	return PBSE_NONE;